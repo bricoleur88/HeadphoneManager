d911482a3e0b12b0b4b21900acbdb030a57a1c9b
진호정 연세대학교 의용전자공학 <jung0984@yonsei.ac.kr>
2020-05-07 16:14:16 +0000

    Django-suit 적용

--- HeadphoneMGR/HeadphoneMGR/settings.py
+++ HeadphoneMGR/HeadphoneMGR/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']    # disallowedhost error 발생으로 수정
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',                                # admin page design 등록
     'django.contrib.admin',                # Django admin app (기본)
     'django.contrib.auth',                 # Django 인증 (기본)
     'django.contrib.contenttypes',         # 다양한 모델데이터 관리 (기본)
@@ -126,3 +127,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+### Django suits 설정###
+
+# Django Suit configuration example
+SUIT_CONFIG = {
+    # header
+     'ADMIN_NAME': '헤드폰 지급/반납 관리자',
+    # 'HEADER_DATE_FORMAT': 'l, j. F Y',
+    # 'HEADER_TIME_FORMAT': 'H:i',
+
+    # forms
+    # 'SHOW_REQUIRED_ASTERISK': True,  # Default True
+    # 'CONFIRM_UNSAVED_CHANGES': True, # Default True
+
+    # menu
+    # 'SEARCH_URL': '/admin/auth/user/',
+    # 'MENU_ICONS': {
+    #    'sites': 'icon-leaf',
+    #    'auth': 'icon-lock',
+    # },
+    # 'MENU_OPEN_FIRST_CHILD': True, # Default True
+    # 'MENU_EXCLUDE': ('auth.group',),
+    # 'MENU': (
+    #     'sites',
+    #     {'app': 'auth', 'icon':'icon-lock', 'models': ('user', 'group')},
+    #     {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
+    #     {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
+    # ),
+
+    # misc
+    # 'LIST_PER_PAGE': 15
+}
